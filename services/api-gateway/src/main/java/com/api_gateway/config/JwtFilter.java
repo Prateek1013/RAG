60701bf05d2fac8905e5ccadef2155e5becd3d23
Prateek1013 <prateek9771196112@gmail.com>
2025-12-21 01:50:42 +0530

    added an api spring cloud gateway

---
+++ services/api-gateway/src/main/java/com/api_gateway/config/JwtFilter.java
@@ -0,0 +1,70 @@
+package com.api_gateway.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class JwtFilter implements GlobalFilter, Ordered {
+    @Value("${security.jwt.secret}")
+    private String secret;
+    private static final List<String> PUBLIC_PATHS = List.of(
+            "/api/auth/login",
+            "/api/auth/register"
+    );
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path=exchange.getRequest().getURI().getPath();
+        if (PUBLIC_PATHS.stream().anyMatch(path::startsWith)) {
+            System.out.println("reached here");
+            return chain.filter(exchange);
+        }
+
+        String authHeader =
+                exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secret.getBytes())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            // Add user info to headers
+            ServerHttpRequest mutatedRequest =
+                    exchange.getRequest().mutate()
+                            .header("X-User-Email", claims.getSubject())
+                            .build();
+
+            return chain.filter(
+                    exchange.mutate().request(mutatedRequest).build()
+            );
+
+        } catch (Exception e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
